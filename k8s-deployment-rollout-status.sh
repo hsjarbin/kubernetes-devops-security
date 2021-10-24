15632f29ce5158ef734c4b39bb10b10d489f9f43
Habiburrokhman Sjarbini <habiburrokhman.sjarbini@gmail.com>
2021-10-24 10:20:47 +0700

    Added emptyDir Vol definitial in k8s_deployment_service.yaml File

--- k8s-deployment-rollout-status.sh
+++ k8s-deployment-rollout-status.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #k8s-deployment-rollout-status.sh
-sleep 30s
+sleep 60s
 
-if [[ $(kubectl -n default rollout status deploy ${deploymentName} --timeout 5s) != *"successfully Rolled Out"* ]];
+if [[ $(kubectl -n default rollout status deploy ${deploymentName} --timeout 5s) != *"successfully rolled out"* ]];
 then
 	echo "Deployment ${deploymentName} Rollout has Failed"
     kubectl -n default rollout undo deploy ${deploymentName}
