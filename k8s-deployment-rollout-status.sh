b944d6e29b5d80306cbce64b1df5ce5a0128fbc6
Habiburrokhman Sjarbini <habiburrokhman.sjarbini@gmail.com>
2021-10-24 09:46:38 +0700

    Applied Workaround for k8s-deployment.sh File

--- k8s-deployment-rollout-status.sh
+++ k8s-deployment-rollout-status.sh
@@ -3,7 +3,7 @@
 #k8s-deployment-rollout-status.sh
 #sleep 60s
 
-if [[ $(kubectl -n default rollout status deploy ${deploymentName} --timeout 5s) != *"successfully rolled out"* ]];
+if [[ $(kubectl -n default rollout status deploy ${deploymentName} --timeout 5s) != *"successfully Rolled Out"* ]];
 then
 	echo "Deployment ${deploymentName} Rollout has Failed"
     kubectl -n default rollout undo deploy ${deploymentName}
